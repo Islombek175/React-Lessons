20d3f17a89111f3115860724acd6d2646b3839f3
islombek <matkaimovislambek@gmail.com2>
2026-01-24 17:49:15 +0500

    hhhhh

---
+++ readME.txt
@@ -0,0 +1,4 @@
+1. npm create vita@latest
+2. React
+3. JavaScript
+
---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+
+function App() {
+	return (
+		<div>
+			<h1>Islombek</h1>
+		</div>
+	)
+}
+export default App
