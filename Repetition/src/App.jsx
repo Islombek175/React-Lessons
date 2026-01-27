6e96f5137b84815e12012d195eddd1f38dd0dcfa
islombek <matkaimovislambek@gmail.com2>
2026-01-27 17:58:18 +0500

    homework

--- Repetition/src/App.jsx
+++ Repetition/src/App.jsx
@@ -3,10 +3,33 @@ import './App.css'
 function App() {
 	return (
 		<div>
-			<div>
-				<img src='' alt='' />
+			<div className='box-1'>
+				<h1>Jiguli blue!</h1>
+				<br />
+				<img src='/juguli_blue.jpg' alt='' />
+				<h3>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta,
+					beatae?
+				</h3>
+				<h3>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta,
+					beatae?
+				</h3>
 			</div>
-			<h1>Islombek</h1>
+
+			<div className='box-2'>
+				<h1>Jiguli pink!</h1>
+				<br />
+				<img src='/juguli_pink.jpg' alt='' width={300} />
+				<h3>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta,
+					beatae?
+				</h3>
+				<h3>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta,
+					beatae?
+				</h3>
+			</div>
 		</div>
 	)
 }
